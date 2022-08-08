92ce8871afda77850b79fe899b439765bb82abdf
mikolajsztaba <mikolaj25sztaba@gmail.com>
2022-08-08 17:14:22 +0200

    Find the nth Digit of a number task

--- Find the nth Digit of a Number/main.py
+++ Find the nth Digit of a Number/main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def find_digit(num, nth):
+    number = str(abs(num))
+    if nth <= 0:
+        return -1
+    elif nth>len(number):
+        return 0
+    else:
+        return int(number[-nth])
